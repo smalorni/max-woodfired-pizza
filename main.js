4dbd9bde1bee0a8984e99919711dfc437f906ea8
Samantha Malorni <samantha.ann.malorni@gmail.com>
2023-08-23 22:09:49 -0400

    full pizza website features

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.getElementById('menuToggle');
-    const menu = document.getElementById('menu');
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav__links');
 
-    menuToggle.addEventListener('click', function() {
-        if (menu.classList.contains('active')) {
-            menu.classList.remove('active');
+    hamburger.addEventListener('click', () => {
+        if (navLinks.classList.contains('active')) {
+            navLinks.classList.remove('active');
+            hamburger.classList.remove('open');
         } else {
-            menu.classList.add('active');
+            navLinks.classList.add('active');
+            hamburger.classList.add('open');
+        }
+    });
+
+    //This was added - to close the menu when a link is clicked
+document.addEventListener('click', (evt) => {
+    if (!navLinks.contains(evt.target) && !hamburger.contains(evt.target)) {
+        navLinks.classList.remove('active');
+        hamburger.classList.remove('open');
         }
     });
 });
